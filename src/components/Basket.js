a3dfde139a4b0dae7273aa0dd7a3f1a96d065f34
vedntzz <vedant.apple@gmail.com>
2022-05-27 16:14:35 +0530

    4th edit

--- src/components/Basket.js
+++ src/components/Basket.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import GooglePayButton from '@google-pay/button-react';
 
 export default function Basket(props) {
   const { cartItems, onAdd, onRemove } = props;
@@ -7,23 +8,23 @@ export default function Basket(props) {
   const shippingPrice = itemsPrice > 2000 ? 0 : 20;
   const totalPrice = itemsPrice + taxPrice + shippingPrice;
   return (
-    <aside className="block col-1">
+    <aside className='block col-1'>
       <h2>Total Item payment</h2>
       <div>
         {cartItems.length === 0 && <div>Cart is empty</div>}
         {cartItems.map((item) => (
-          <div key={item.id} className="row">
-            <div className="col-2">{item.name}</div>
-            <div className="col-2">
-              <button  onClick={() => onRemove(item)} className="remove">
+          <div key={item.id} className='row'>
+            <div className='col-2'>{item.name}</div>
+            <div className='col-2'>
+              <button onClick={() => onRemove(item)} className='remove'>
                 -
               </button>{' '}
-              <button  onClick={() => onAdd(item)} className="add">
+              <button onClick={() => onAdd(item)} className='add'>
                 +
               </button>
             </div>
 
-            <div className="col-2 text-right">
+            <div className='col-2 text-right'>
               {item.qty} x Rs. {item.price.toFixed(2)}
             </div>
           </div>
@@ -32,34 +33,91 @@ export default function Basket(props) {
         {cartItems.length !== 0 && (
           <>
             <hr></hr>
-            <div className="row">
-              <div className="col-2">Items Price</div>
-              <div className="col-1 text-right">Rs. {itemsPrice.toFixed(2)}</div>
+            <div className='row'>
+              <div className='col-2'>Items Price</div>
+              <div className='col-1 text-right'>
+                Rs. {itemsPrice.toFixed(2)}
+              </div>
             </div>
-            <div className="row">
-              <div className="col-2">Tax Price</div>
-              <div className="col-1 text-right">Rs.{taxPrice.toFixed(2)}</div>
+            <div className='row'>
+              <div className='col-2'>Tax Price</div>
+              <div className='col-1 text-right'>Rs.{taxPrice.toFixed(2)}</div>
             </div>
-            <div className="row">
-              <div className="col-2">Shipping Price</div>
-              <div className="col-1 text-right">
+            <div className='row'>
+              <div className='col-2'>Shipping Price</div>
+              <div className='col-1 text-right'>
                 Rs. {shippingPrice.toFixed(2)}
               </div>
             </div>
 
-            <div className="row">
-              <div className="col-2">
+            <div className='row'>
+              <div className='col-2'>
                 <strong>Total Price</strong>
               </div>
-              <div className="col-1 text-right">
+              <div className='col-1 text-right'>
                 <strong>Rs. {totalPrice.toFixed(2)}</strong>
               </div>
             </div>
             <hr />
-            <div className="row">
-              <button className='payment ' onClick={() => alert('Implement Checkout!')}>
+            <div
+              className='row'
+              style={{
+                display: 'flex',
+                flexDirection: 'column',
+                alignItems: 'center',
+                justifyContent: 'center',
+                marginTop: '15px',
+              }}
+            >
+              {/* <button className='payment ' onClick={() => alert('Implement Checkout!')}>
                 Checkout
-              </button>
+              </button> */}
+
+              <GooglePayButton
+                environment='TEST'
+                paymentRequest={{
+                  apiVersion: 2,
+                  apiVersionMinor: 0,
+                  allowedPaymentMethods: [
+                    {
+                      type: 'CARD',
+                      parameters: {
+                        allowedAuthMethods: ['PAN_ONLY', 'CRYPTOGRAM_3DS'],
+                        allowedCardNetworks: ['MASTERCARD', 'VISA'],
+                      },
+                      tokenizationSpecification: {
+                        type: 'PAYMENT_GATEWAY',
+                        parameters: {
+                          gateway: 'example',
+                          gatewayMerchantId: 'exampleGatewayMerchantId',
+                        },
+                      },
+                    },
+                  ],
+                  merchantInfo: {
+                    merchantId: '12345678901234567890',
+                    merchantName: 'Demo Merchant',
+                  },
+                  transactionInfo: {
+                    totalPriceStatus: 'FINAL',
+                    totalPriceLabel: 'Total',
+                    totalPrice: `${totalPrice}`,
+                    currencyCode: 'USD',
+                    countryCode: 'US',
+                  },
+                  shippingAddressRequired: false,
+                  callbackIntents: ['PAYMENT_AUTHORIZATION'],
+                }}
+                onLoadPaymentData={(paymentRequest) => {
+                  console.log('load payment data', paymentRequest);
+                }}
+                onPaymentAuthorized={(paymentData) => {
+                  console.log('Payment Authorised Success', paymentData);
+                  return { transactionState: 'SUCCESS' };
+                }}
+                existingPaymentMethodRequired='false'
+                buttonType='Buy'
+              />
             </div>
           </>
         )}
